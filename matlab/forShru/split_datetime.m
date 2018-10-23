a9187491fa6483b0f55cdaf56da381188c28acd2
Deepak <deepu.sharma@gmail.com>
2018-10-23 12:24:57 -0700

    .

--- matlab/forShru/split_datetime.m
+++
@@ -1,8 +0,0 @@
-function datetime_tab = split_datetime (datetimeval, name)   
-  time_part = datestr(datetimeval, 'HH:MM:SS');
-  date_part = datestr(datetimeval, 'yyyy-mm-dd');
-
-  datetime_tab = table (date_part,time_part);
-  datetime_tab.Properties.VariableNames = {horzcat(name,'_date_part'), horzcat(name,'_time_part')};
-end  
-  
